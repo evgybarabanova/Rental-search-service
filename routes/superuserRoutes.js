4f8a82803f373dde66f4d8684c3f71f16f42c748
Akma <napoli-kz@mail.ru>
2022-03-15 15:37:15 +0200

    regchanges

--- routes/superuserRoutes.js
+++ routes/superuserRoutes.js
@@ -1,5 +1,7 @@
 const express = require('express');
     const router = express.Router();
+    const router = express.Router();
+const {Superuser} = require('../db/models');
     
     router.get('/registation', (req, res) => {
   res.render('user/registation');
